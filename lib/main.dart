d7c0780bf834fcac971dc23758188635412e00f6
0X7FFFFFFF <563748846@qq.com>
2022-09-24 10:39:46 -0600

    jsonUpdate

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_displaymode/flutter_displaymode.dart';
+import 'dart:io' show Platform;
+import 'nav.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-    statusBarColor: Colors.transparent,
-    systemNavigationBarColor: Colors.transparent,
-    systemNavigationBarDividerColor: Colors.transparent,
+  if (Platform.isAndroid) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      systemNavigationBarColor: Colors.transparent,
+      systemNavigationBarDividerColor: Colors.transparent,
+    ));
+  }
+  runApp(MultiProvider(
+    providers: [],
+    child: MyApp(),
   ));
-  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
@@ -18,43 +27,43 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'wows flutter',
-      theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: const Color(0xff6750a4), // M3 Baseline
-      ),
-      home: const HomePage(),
-    );
+  final routes = {
+    '/': (context, {arguments}) => HomePage(),
+  };
+
+  Future<dynamic> highRefreshRate() async {
+    // 高刷新率兼容
+    try {
+      await FlutterDisplayMode.setHighRefreshRate();
+    } on Exception catch (e) {
+      /// e.code =>
+      /// noAPI - No API support. Only Marshmallow and above.
+      /// noActivity - Activity is not available. Probably app is in background
+      // ignore: avoid_print
+      print(e.toString());
+    }
+    return 'OK';
   }
-}
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add),
-      ),
-      appBar: AppBar(
-        title: const Text('TEST'),
-      ),
-      body: const Center(
-        child: Card(
-          color: Colors.redAccent,
-          child: Text("TEST"),
-        ),
-      ),
+    return FutureBuilder(
+      future: highRefreshRate(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.hasData) {
+          return MaterialApp(
+            title: 'wows flutter',
+            theme: ThemeData(
+              useMaterial3: true,
+              colorSchemeSeed: const Color(0xff6750a4), // M3 Baseline
+            ),
+            routes: routes,
+            initialRoute: '/',
+          );
+        } else {
+          return const MaterialApp();
+        }
+      },
     );
   }
 }
---
+++ lib/model/ship.dart
@@ -0,0 +1,56 @@
+// ignore_for_file: non_constant_identifier_names
+
+class Ship {
+  late int id;
+  late int tier;
+  late String type;
+  late bool is_special;
+  late bool is_premium;
+  late String nation;
+  late String name;
+  String getRomanTier() {
+    var romanList = [
+      "0",
+      "I",
+      "II",
+      "III",
+      "IV",
+      "V",
+      "VI",
+      "VII",
+      "VIII",
+      "IX",
+      "X",
+      "★"
+    ];
+    try {
+      return romanList[tier];
+    } catch (e) {
+      return "Unknow";
+    }
+  }
+
+  int getShipID() {
+    return id;
+  }
+
+  String getType() {
+    return type;
+  }
+
+  String getName() {
+    return name;
+  }
+
+  String getNation() {
+    return nation;
+  }
+
+  bool getSpecial() {
+    return is_special;
+  }
+
+  bool getPremium() {
+    return is_premium;
+  }
+}
